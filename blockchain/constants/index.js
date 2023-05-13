f0ef5ed3628a8ef324c0872c296d0e5c1935d456
Emmanuel Ifeanyi MECHIE <64092981+emmanuelByte@users.noreply.github.com>
2023-05-13 15:58:46 +0100

    Update index.js

--- blockchain/constants/index.js
+++ blockchain/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0xF41081BE1E9885eCCF3d18443176cb619e4C261c';
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'https://nft-collection-emanuelone.vercel.app/api/';
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
